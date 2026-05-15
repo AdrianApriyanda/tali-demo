3c4ff50304f20599f2809aae099ccd1a1ce0c6de
AdrianApriyanda <111054585+AdrianApriyanda@users.noreply.github.com>
2026-05-15 17:42:50 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/tali-demo/' // ⚠️ MUST MATCH YOUR REPO NAME
+  base: '/tali-demo/'
 })
